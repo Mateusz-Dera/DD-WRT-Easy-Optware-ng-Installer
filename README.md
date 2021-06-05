9a7eaa8389f09fe53ed408a4116f0f824bd601f1
Mateusz Dera <OtahoPL@gmail.com>
2021-06-05 18:45:16 +0200

    NVRAM

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
-# DD-WRT Easy Optware-ng Installer
+## DD-WRT Easy Optware-ng Installer
 
-# Info
-  - This script install Optware-ng, create and mount /opt parttion in /jffs/opt and add mount to autostart
-  - Tested on Netgear R6400v2 (DD-WRT v3.0-r37305)
+### Info:
+  - This script install Optware-ng create and mount **/opt** parttion in **/jffs/opt** 
+  - NVRAM script mount **/opt** on start
+  - Tested on Netgear R6400v2 (DD-WRT v3.0-r46772 std)
 
-# USB Requirements
+### USB Requirements:
  - Mounted JFFS partition
  - Optional SWAP partition
 
-# Installation
+### Installation:
 ```sh
 cd /jffs
-curl -kLO https://raw.githubusercontent.com/Mateusz-Dera/DD-WRT-Easy-Optware-ng-Installer/DD-WRT-v3.0-r37305/install.sh
+curl -kLO https://raw.githubusercontent.com/Mateusz-Dera/DD-WRT-Easy-Optware-ng-Installer/master/install.sh
 sh ./install.sh
 reboot
+```
+
+### Test:
+```sh
+/opt/bin/ipkg update
+/opt/bin/ipkg install nano
 ```
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # DD-WRT DD-WRT Easy Optware-ng Installer
-# Copyright © 2019 Mateusz Dera
+# Copyright © 2019-2021 Mateusz Dera
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,44 +21,33 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-echo -e "\e[92;1;48;5;239m ====================================== \e[0m"
-echo -e "\e[92;1;48;5;240m |  DD-WRT EASY OPTWARE-NG INSTALLER  | \e[0m"
-echo -e "\e[92;1;48;5;241m |  \e[94;1;48;5;241mMateusz Dera  \e[92;1;48;5;241m                    | \e[0m"
-echo -e "\e[92;1;48;5;240m | \e[94;1;48;5;240m Version:\e[92;1;48;5;240m 1.3                      | \e[0m"
-echo -e "\e[92;1;48;5;239m ====================================== \e[0m"
-
-echo
-
-cd /jffs || exit 1
+cd /jffs
 
 if ! [ -d "/jffs/opt" ]; then
-   mkdir /jffs/opt || exit 1
-fi
-
-if ! [ -d "/jffs/etc" ]; then
-   mkdir /jffs/etc || exit 1
-fi
-
-if ! [ -d "/jffs/etc/config" ]; then
-   mkdir /jffs/etc/config || exit 1
+   mkdir /jffs/opt
 fi
 
 mount -o bind /jffs/opt /opt
 
+echo -e "\033[0;32mDD-WRT Easy Optware-ng Installer"
+echo -e "Copyright © 2019-2021 Mateusz Dera"
+echo -e "\033[1;33m"
+echo -e "Detected architecture:"
+uname -m
+echo -e "\033[0;32m"
+
 if ! [ -x "$(command -v /opt/bin/ipkg update)" ]; then
-   echo -e "\e[92;1;48;5;239m ================================ \e[0m"
-   echo -e "\e[92;1;48;5;240m |  SELECT ROUTER ARCHITECTURE  | \e[0m"
-   echo -e "\e[92;1;48;5;242m | \e[94;1;48;5;242m 0\e[92;1;48;5;242m ARMv7 EABI hardfloat      | \e[0m"
-   echo -e "\e[92;1;48;5;243m | \e[94;1;48;5;243m 1\e[92;1;48;5;243m ARMv7 EABI softfloat      | \e[0m"
-   echo -e "\e[92;1;48;5;244m | \e[94;1;48;5;244m 2\e[92;1;48;5;244m ARMv5 EABI                | \e[0m"
-   echo -e "\e[92;1;48;5;245m | \e[94;1;48;5;245m 3\e[92;1;48;5;245m ARMv5 EABI legacy         | \e[0m"
-   echo -e "\e[92;1;48;5;244m | \e[94;1;48;5;244m 4\e[92;1;48;5;244m MIPSEL                    | \e[0m"
-   echo -e "\e[92;1;48;5;243m | \e[94;1;48;5;243m 5\e[92;1;48;5;243m PowerPC 603e              | \e[0m"
-   echo -e "\e[92;1;48;5;242m | \e[94;1;48;5;242m 6\e[92;1;48;5;242m PowerPC e500v2            | \e[0m"
-   echo -e "\e[92;1;48;5;241m | \e[94;1;48;5;241m 7\e[92;1;48;5;241m PowerPC I686              | \e[0m"
-   echo -e "\e[92;1;48;5;240m | \e[94;1;48;5;240m 8\e[92;1;48;5;240m PowerPC x86_64            | \e[0m"
-   echo -e "\e[92;1;48;5;239m ================================ \e[0m\n"
+   echo -e "0 ARMv7 EABI hardfloat"
+   echo -e "1 ARMv7 EABI softfloat"
+   echo -e "2 ARMv5 EABI"
+   echo -e "3 ARMv5 EABI legacy"
+   echo -e "4 MIPSEL"
+   echo -e "5 PowerPC 603e"
+   echo -e "6 PowerPC e500v2"
+   echo -e "7 PowerPC I686"
+   echo -e "8 PowerPC x86_64"
    read -p $'Number (Default 0): ' arch
+   echo -e "\033[0m"
 
    case $arch in
    "1") link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/buildroot-armeabi-ng-bootstrap.sh" ;;
@@ -73,22 +62,10 @@ if ! [ -x "$(command -v /opt/bin/ipkg update)" ]; then
    esac
   
    wget -O - $link | sh || exit 1
-
 fi
 
-export PATH=$PATH:/opt/bin:/opt/sbin || exit 1
-/opt/bin/ipkg update || exit 1
+export PATH=$PATH:/opt/bin:/opt/sbin
+/opt/bin/ipkg update
 
-/opt/bin/ipkg install coreutils || exit 1 
-
-cd /jffs/etc/config || exit 1
-[ -f ./automatic_opt_mount.startup ] && rm automatic_opt_mount.startup
-[ -f ./automatic_opt_mount.startup ] && exit 1
-echo -e '#!/bin/sh\n/usr/bin/logger -t START_$(basename $0) "started [$@]"\nSCRLOG=/tmp/$(basename $0).log\ntouch $SCRLOG\nTIME=$(date +"%Y-%m-%d %H:%M:%S")\necho $TIME "$(basename $0) script started [$@]" >> $SCRLOG\nmount -o bind /jffs/opt/ /opt/ \nTIME=$(date +"%Y-%m-%d %H:%M:%S")\nif [ "$?" -ne 0 ]\nthen\necho $TIME "Error in script execution! Script: $0" >> $SCRLOG\nelse\necho $TIME "Script execution OK. Script: $0" >> $SCRLOG\nfi\n/usr/bin/logger -t STOP_$(basename $0) "return code $?"\nexit $?' > automatic_opt_mount.startup || exit 1
-[ -f ./automatic_opt_mount.startup ] || exit 1
-chmod 700 automatic_opt_mount.startup || exit 1
-
-case $1 in
-   "-s") exit 0;;
-   *) echo -e "Installation complete!\nRestart router"
-esac
+nvram set rc_startup="mount -o bind /jffs/opt /opt"
+nvram commit
